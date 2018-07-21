4aadcac34628a09b7a30c3a60a521ea8dee94535
ChanoHL <shanohl.sist@gmail.com>
2018-07-21 10:40:08 -0500

    Configuration and creation models

---
+++ Search.lib.net/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search.lib.net
+{
+    public class Class1
+    {
+    }
+}
